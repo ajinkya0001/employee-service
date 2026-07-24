9ca8e31fca28ad579c2f884e84ca6f99136e03bb
ajinkya0001 <jadkarajinkya@gmail.com>
2026-07-24 15:58:12 +0530

    Version 4 : checking the rolling deployment

--- src/main/java/com/example/employeeservice/controller/EmployeeController.java
+++ src/main/java/com/example/employeeservice/controller/EmployeeController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @RestController
@@ -14,9 +15,27 @@ public class EmployeeController {
     @GetMapping("/employees")
     public List<Employee> getEmployees() {
         return List.of(
-                new Employee(1L, "Ajinkya version 3", "DevOps"),
-                new Employee(2L, "Rahul version 3", "Development"),
-                new Employee(3L, "Priya version 3", "Testing")
+                new Employee(1L, "Ajinkya 🚀 CI/CD Build #2", "Senior DevOps"),
+                new Employee(2L, "GitHub Webhook Triggered", "Jenkins"),
+                new Employee(3L, "Rolling Deployment Successful", "Amazon ECS")
         );
+    }
+
+    @GetMapping("/version")
+    public String version() {
+        return "Employee Service Version 2 | Deployed at: " + LocalDateTime.now();
+    }
+    @GetMapping("/deployment")
+    public String deployment() {
+        return """
+            Deployment Details
+
+            Application : Employee Service
+            Version     : v2
+            Environment : Production
+            Platform    : AWS ECS Fargate
+            CI/CD       : Jenkins + GitHub Webhook
+            Status      : Running Successfully
+            """;
     }
 }
